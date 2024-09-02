0ed7020e466fc4a1b3129fde4718021c317df54a
marco.piazzola <marco.piazzola@spindox.it>
2024-09-02 17:29:09 +0200

    added page showing the ticket/appointment number

---
+++ src/routes/[appId]/ticket/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import {  onMount } from "svelte";
+
+  let ticketNumber: string | null = ''
+  let appointmentId: string | null = ''
+
+  onMount(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    ticketNumber = urlParams.get('ticketNumber');
+    appointmentId = urlParams.get('appointmentId');
+  })
+
+  const goHome = () => {
+    window.location = window.location.origin
+  }
+
+</script>
+
+<div class="ticket-content">
+{#if ticketNumber}
+  Your ticket number is: <span>{ticketNumber}</span>
+{/if}
+{#if appointmentId}
+  Your appointment number is: <span>{appointmentId}</span>
+{/if}
+<div class="mt-5">
+  <button  class="button is-text" on:click={()  => goHome()}>Go home</button>
+</div>
+</div>
+
+<style>
+.ticket-content{
+  padding-top: 20px;
+  background-color: var(--theme-primary-bg-color);
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  font-size: 1.5em;
+}
+.ticket-content span{
+  font-weight: bold;
+}
+
+</style>

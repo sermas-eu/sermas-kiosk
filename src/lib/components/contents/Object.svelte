3d36655db3e6014f8427051b7481845ae06da220
Giulia Costa <giulia.costa@spindox.it>
2024-12-16 12:16:28 +0100

    init 3d

--- src/lib/components/contents/Object.svelte
+++ src/lib/components/contents/Object.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
-    import { type ObjectContentDto } from "@sermas/toolkit";
+  import { type ObjectContentDto } from "@sermas/toolkit";
+  import { Canvas } from "@threlte/core";
+  import Scene from "./Scene.svelte";
 
-    export let content: ObjectContentDto;
+  export let content: ObjectContentDto;
 </script>
 
 <span class="object">
-    TODO Object {content.url}
+  <div class="three-d">
+    <Canvas>
+      <Scene type={content.type} url={content.url} />
+    </Canvas>
+  </div>
 </span>
 
-<style>
+<style lang="scss">
+  .three-d {
+    position: relative;
+    width: 99%;
+  }
 </style>
---
+++ src/lib/components/contents/Scene.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { T } from "@threlte/core";
+  import { GLTF, OrbitControls } from "@threlte/extras";
+  import { interactivity } from "@threlte/extras";
+
+  interactivity();
+  export let type;
+  export let url: string;
+</script>
+
+<T.PerspectiveCamera makeDefault position={[5, 2, 5]} fov={25}>
+  <OrbitControls autoRotate enableDamping />
+</T.PerspectiveCamera>
+
+<T.AmbientLight />
+
+{#if type === "glb"}
+  <GLTF {url} interactive />
+{:else if type === "obj"}
+  <GLTF
+    {url}
+    useDraco="https://www.gstatic.com/draco/versioned/decoders/1.5.7/*"
+  />
+{/if}

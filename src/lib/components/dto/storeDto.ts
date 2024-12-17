307a7274a79fc2f2169ace16aa4801b08fc4d77c
Kanthavel Pasupathipillai <kanthavel.pasupathipillai@spindox.it>
2024-12-17 11:12:00 +0100

    Renamed oldImage to previousImage and added comment

--- src/lib/components/dto/storeDto.ts
+++ src/lib/components/dto/storeDto.ts
@@ -3,7 +3,7 @@ import { SupportedAudioTypes } from "@sermas/api-client";
 export interface BackgroundStoreDto {
   image: string | undefined; // "stream" -> images that comes from messages
   urlImage: string;
-  oldImage?: string;
+  previousImage?: string;  // In case we want "image" only applied to specific pages, this attribute allows us to go back to the previous image 
   messageImage: boolean;
   backgroundAudioUrl: string | undefined;
   backgroundAudioType: SupportedAudioTypes | undefined;

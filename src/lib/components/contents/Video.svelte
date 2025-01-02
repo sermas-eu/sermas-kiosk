0ed17af5c74b7644d2d82f97927f0e62998c1525
Giulia Costa <giulia.costa@spindox.it>
2025-01-02 18:07:45 +0100

    add type to video

--- src/lib/components/contents/Video.svelte
+++ src/lib/components/contents/Video.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
-    import { type VideoContentDto } from "@sermas/toolkit";
+  import { type VideoContentDto } from "@sermas/toolkit";
 
-    export let content: VideoContentDto;
+  export let content: VideoContentDto;
 </script>
 
 <span class="video">
-    <video>
-        {#each content.sources as source}
-            <source src={source} />
-        {/each}
-    </video>
+  <!-- svelte-ignore a11y-media-has-caption -->
+  <video>
+    {#each content.sources as source}
+      <source src={source} type={content.type} />
+    {/each}
+  </video>
 </span>
 
 <style>

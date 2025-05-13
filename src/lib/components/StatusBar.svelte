28296f7eb0ea9000908b966a299f34db273596dc
lc-spxl <luca.capra@spindox.it>
2025-05-13 17:08:21 +0200

    move to system messages, add ui events

--- src/lib/components/StatusBar.svelte
+++ src/lib/components/StatusBar.svelte
@@ -1,27 +1,82 @@
 <script lang="ts">
     import { browser } from "$app/environment";
     import { toolkit } from "$lib";
-    import type { SystemProgressEvent, UiStatus } from "@sermas/toolkit/dto";
-    import { sendStatus } from "@sermas/toolkit/events";
+    import type {
+        RequestProcessing,
+        SystemProgressEvent,
+        UiStatus,
+        UserSpeaking,
+    } from "@sermas/toolkit/dto";
     import { onDestroy, onMount } from "svelte";
-    import SystemProgress from "./SystemProgress.svelte";
 
     export let system = "";
-    let progress = "";
+
+    let userSpeaking = false;
 
     const onStatus = (ev: UiStatus) => {
-        console.warn(ev);
         system = ev.message;
     };
 
     const onProgress = (ev: SystemProgressEvent) => {
-        console.warn(ev);
-        progress = ev.event;
+        // system = "";
+        // progress = ev.event;
+        switch (ev.event) {
+            case "stt":
+                system = "Processing audio";
+                break;
+            case "analyze":
+                system = "Analyzing audio";
+                break;
+            case "llm":
+                system = "Reasoning";
+                break;
+            case "translate":
+                system = "Translating";
+                break;
+            case "tts":
+                system = "Answering";
+                break;
+            case "ended":
+                system = "";
+                break;
+            default:
+                system = ev.event;
+                break;
+        }
+    };
+
+    const onUserSpeaking = (ev: UserSpeaking) => {
+        switch (ev.status) {
+            case "speaking":
+                system = "Listening";
+                userSpeaking = true;
+                break;
+            case "noise":
+                if (!userSpeaking) system = "Noise detected";
+                break;
+            case "completed":
+                userSpeaking = false;
+                system = "";
+                break;
+        }
+    };
+
+    const onAvatarSpeaking = (isSpeaking: boolean) => {
+        if (!userSpeaking && isSpeaking) system = "Avatar speaking";
+    };
+
+    const onRequestProcessing = (ev: RequestProcessing) => {
+        if (ev.status === "started") {
+            system = "Sending request";
+        }
     };
 
     onMount(() => {
         if (!browser) return;
         toolkit.getUI().on("ui.status", onStatus);
+        toolkit.getUI().on("ui.avatar.speaking", onAvatarSpeaking);
+        toolkit.getUI().on("ui.user.speaking", onUserSpeaking);
+        toolkit.getUI().on("ui.user.request-processing", onRequestProcessing);
         toolkit.getUI().on("dialogue.progress", onProgress);
     });
 
@@ -33,7 +88,6 @@
 
 <div class="status-bar px-5 text-yellow-500" id="status-bar">
     <div class="system">{system || ""}</div>
-    <div class="system"><SystemProgress event={progress} /></div>
 </div>
 
 <style lang="scss">
